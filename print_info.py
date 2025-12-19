5e2c0dab82491b94aa258acd428be60846c5734f
Youngki Baik <hyunxx@gmail.com>
2025-12-19 21:28:07 +0900

    print_info updated

--- print_info.py
+++ print_info.py
@@ -13,18 +13,41 @@ def get_target_keys(data: dict, developer_selection: list) -> list:
     return out_list
 
 
+def data_read(file_name: str, developer_selection: list) -> tuple[list, dict]:
+    with open(file_name, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        data_keys = get_target_keys(data, developer_selection)
+
+    return data_keys, data
+
+
+def put_data_to_table(key: str, data: dict, table: Table, developer_selection: list):
+    v = data[key]
+    developer = v.get("developer", "N/A").lower()
+    if len(developer_selection) and developer not in developer_selection:
+        pass
+    else:
+        table.add_row(
+            key,
+            v.get("developer", "N/A"),
+            v.get("platform", "N/A"),
+            v.get("localization", "N/A"),
+        )
+
+
 def main():
+    platform_selection = ["dos", "pc98"]
     developer_selection = []
 
-    platform = "dos"
-    with open(f"./list_{platform}.json", "r", encoding="utf-8") as f:
-        data_dos = json.load(f)
-        dos_list = get_target_keys(data_dos, developer_selection)
+    dos_list = []
+    pc98_list = []
+    data_dos = {}
+    data_pc98 = {}
 
-    platform = "pc98"
-    with open(f"./list_{platform}.json", "r", encoding="utf-8") as f:
-        data_pc98 = json.load(f)
-        pc98_list = get_target_keys(data_pc98, developer_selection)
+    if "dos" in platform_selection:
+        dos_list, data_dos = data_read("./list_dos.json", developer_selection)
+    if "pc98" in platform_selection:
+        pc98_list, data_pc98 = data_read("./list_pc98.json", developer_selection)
 
     target_list = list(set(dos_list) | set(pc98_list))
     target_list.sort()
@@ -37,30 +60,13 @@ def main():
     table.add_column("Localization", justify="center", style="cyan")
 
     for k in target_list:
-        if k in dos_list:
-            v = data_dos[k]
-            developer = v.get("developer", "N/A").lower()
-            if len(developer_selection) and developer not in developer_selection:
-                pass
-            else:
-                table.add_row(
-                    k,
-                    v.get("developer", "N/A"),
-                    v.get("platform", "N/A"),
-                    v.get("localization", "N/A"),
-                )
-        if k in pc98_list:
-            v = data_pc98[k]
-            developer = v.get("developer", "N/A").lower()
-            if len(developer_selection) and developer not in developer_selection:
-                pass
-            else:
-                table.add_row(
-                    k,
-                    v.get("developer", "N/A"),
-                    v.get("platform", "N/A"),
-                    v.get("localization", "N/A"),
-                )
+        if "dos" in platform_selection:
+            if k in dos_list:
+                put_data_to_table(k, data_dos, table, developer_selection)
+
+        if "pc98" in platform_selection:
+            if k in pc98_list:
+                put_data_to_table(k, data_pc98, table, developer_selection)
 
     console.print(table)
 
